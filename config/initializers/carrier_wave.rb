136eacc8a6cdbfff41bbd2151cedda342f507459
takuya kinoshita <kktak02@gmail.com>
2021-04-04 16:26:11 +0900

    aws delete fog

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,12 @@
 if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Amazon S3用の設定
-      :provider              => 'AWS',
-      :region                => ENV['AWS_REGION'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['AWS_SECRET_KEY']
-    }
-    config.fog_directory     =  ENV['AWS_BUCKET']
-  end
+#  CarrierWave.configure do |config|
+#    config.fog_credentials = {
+#      # Amazon S3用の設定
+#      :provider              => 'AWS',
+#      :region                => ENV['AWS_REGION'],     # 例: 'ap-northeast-1'
+#      :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
+#      :aws_secret_access_key => ENV['AWS_SECRET_KEY']
+#    }
+#    config.fog_directory     =  ENV['AWS_BUCKET']
+#  end
 end
